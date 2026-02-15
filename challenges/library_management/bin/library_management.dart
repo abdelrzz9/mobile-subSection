fe34243e8376001e7eab56224eb0119d86c3dd1d
abdelrzz9 <ladjiciabdelrazzak@gmail.com>
2026-02-16 00:05:11 +0100

    add: initial library management module

---
+++ challenges/library_management/bin/library_management.dart
@@ -0,0 +1,5 @@
+import 'package:library_management/library_management.dart' as library_management;
+
+void main(List<String> arguments) {
+  print('Hello world: ${library_management.calculate()}!');
+}
---
+++ challenges/library_management/test/library_management_test.dart
@@ -0,0 +1,8 @@
+import 'package:library_management/library_management.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
