5e60c1d675e4a460a8e6b9e0617b65689b520e65
abdelrzz9 <ladjiciabdelrazzak@gmail.com>
2026-02-17 00:33:08 +0100

    add: some function to challenge it not the finle output

--- challenges/library_management/bin/library_management.dart
+++ challenges/library_management/bin/library_management.dart
@@ -1,5 +1,100 @@
-import 'package:library_management/library_management.dart' as library_management;
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+import 'dart:isolate';
 
-void main(List<String> arguments) {
-  print('Hello world: ${library_management.calculate()}!');
+enum BookStatus { available, loaned, reserved }
+
+enum MemberTier { standard, premium }
+
+class BOOK {
+  String? id;
+  String? title;
+  String? type;
+  int? page;
+  BOOK(this.title, this.page, this.type);
 }
+
+class USER {
+  String? id;
+  String? name;
+  MemberTier tier = MemberTier.standard;
+  USER(this.name);
+}
+
+List<BOOK> books = [];
+List<USER> users = [];
+
+BOOK? findBook(String? title) {
+  if (title == null) {
+    return null;
+  }
+  try {
+    return books.firstWhere((e) => e.title == title);
+  } catch (e) {
+    return null;
+  }
+}
+
+// void addBook() {
+//   BOOK? b;
+//   b.title = stdin.readLineSync();
+//   b.page = stdin.readLineSync();
+//   b.type = stdin.readLineSync();
+// }
+
+void deletBook(String? title) {
+  if (title == null) return;
+  books.removeWhere((e) => e.title == title);
+}
+
+List<BOOK>? getBooksByName(String? title) {
+  try {
+    return books.where((e) => e.title == title).toList();
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<void> displayBooks() async {
+  print('============');
+  for (var book in books) {
+    print(book.title);
+    print(book.type);
+    print('============');
+  }
+}
+
+Future<USER?> findUsre(String? name) async {
+  if (name == null) return null;
+  try {
+    users.firstWhere((e) => e.name == name);
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<void> deleteUser(String? name) async {
+  if (name == null) return;
+  users.removeWhere((e) => e.name == name);
+}
+
+Future<void> displayUser() async {
+  print('=========');
+  for (var user in users) {
+    print(user.name);
+    print('=========');
+  }
+}
+
+Future<void> upgraidUser(String? name) async {
+  final user = await findUsre(name);
+  if (user == null) {
+    return;
+  }
+  if (user.tier == MemberTier.standard) {
+    user.tier == MemberTier.premium;
+  }
+}
+
+void main() {}
